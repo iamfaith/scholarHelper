880d60b7b30d3710026ac1430f72a20a2e5b079a
xianzixiang <xianzixiang@wps.cn>
2019-01-10 21:54:06 +0800

    auto

--- js/content-script.js
+++ js/content-script.js
@@ -1,8 +1,18 @@
+let parseRanking = function (data) {
+    data = data.substring('noCB('.length, data.length - 1)
+    data = JSON.parse(data)
+    return data["response"]
+}
+
+
 let customPanel = function () {
     let panel = document.createElement('div');
     panel.className = 'chrome-plugin-demo-panel';
     panel.innerHTML = `
 		<h2>Please input:</h2>
+		<div class="btn-area">
+            <input id="schoolInput" placeholder="input school first"/>
+		</div>
 		<div class="btn-area">
             <input id="customInput" />
             <button id="customBtn">search</button>
@@ -11,20 +21,84 @@ let customPanel = function () {
 		</div>
 	`;
     document.body.appendChild(panel);
+    $(document.body).append('<IFRAME id="scholarIfm" class="scholar-bubble"></IFRAME>');
+    $('#scholarIfm').hide()
+
+    $('#schoolInput').on('input', function () {
+        let kw = $('#schoolInput').val()
+        $.get('https://search-production.ratemyprofessors.com/solr/rmp/select/?solrformat=true&rows=20&wt=json&json.wrf=noCB&callback=noCB&q=' + kw + '&defType=edismax&bq=schoolname_sort_s%3A%22University+of+Manito%22%5E1000&qf=schoolname_autosuggest&bf=pow(total_number_of_ratings_i%2C1.9)&sort=score+desc&siteName=rmp&rows=20&group=off&group.field=content_type_s&group.limit=20&fq=content_type_s%3ASCHOOL', function (data) {
+            let resp = parseRanking(data)["docs"]
+            let school = resp.map(item => {
+                let id = item["id"].replace("school", "schoolid_s")
+                return {label: item['schoolname_s'], value: id}
+            })
+            $('#schoolInput').autocomplete({
+                source: school
+            });
+            //console.log(school, kw)
+        })
+
+    })
     $('#customBtn').click(function () {
         let kw = $('#customInput').val()
         let ret = $("a[href*='" + kw + "']").filter(function () {
-            return this.text.indexOf('@') === -1
+            return this.text.indexOf('@') === -1 && this.text.indexOf(',') !== -1
         })
         ret.each(function (i, item) {
             i = $.trim(item.text)
-            console.log(i, kw)
-            $(item.parentNode).append('<button showMore="true" name=' + i + ' >more</button>')
+            let parent = $(item.parentNode)
+            // $.get("https://search-production.ratemyprofessors.com/solr/rmp/select/?solrformat=true&rows=20&wt=json&json.wrf=noCB&callback=noCB&q=" + i + "&defType=edismax&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=total_number_of_ratings_i+desc&siteName=rmp&rows=20&start=0&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s&fq=", function (data) {
+            $.get("https://search-production.ratemyprofessors.com/solr/rmp/select/?solrformat=true&rows=20&wt=json&json.wrf=noCB&callback=noCB&q=" + i + "&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+teacherfullname_autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=score+desc&defType=edismax&siteName=rmp&rows=20&group=off&group.field=content_type_s&group.limit=20&fq=" + $('#schoolInput').val(), function (data) {
+                let resp = parseRanking(data)
+                console.log(i, resp)
+                let rate = "", pkid = ""
+
+                if (resp["numFound"] > 0) {
+                    let docs = resp["docs"];
+                    pkid = docs[0]["pk_id"]
+                    docs = docs.map(item => {
+                        return {
+                            //"average clarity": item["averageclarityscore_rf"],
+                            "难度": item["averageeasyscore_rf"],
+                            "有用程度": item["averagehelpfulscore_rf"],
+                            "平均热分": item["averagehotscore_rf"],
+                            "学生评分": item["averageratingscore_rf"],
+                            "标签": item["tag_s_mv"],
+                            "评分数": item["total_number_of_ratings_i"],
+                        }
+                    })
+                    // averageclarityscore_rf: 1.85
+                    // averageeasyscore_rf: 3.5
+                    // averagehelpfulscore_rf: 1.73
+                    // averagehotscore_rf: -13
+                    // averageratingscore_rf: 1.79
+                    //tag_s_mv
+                    //total_number_of_ratings_i
+                    rate = JSON.stringify(docs)
+                } else if (resp["numFound"] > 1) {
+                    console.log('多个结果')
+                }
+                parent.append('<button showMore="true" name="' + i + '" pkid="' + pkid + '">more</button>')
+                if (rate !== "")
+                    parent.append('<div style="width: 100%; word-wrap: break-word">' + rate + '</div>')
+            })
         })
     });
 
-    $('button[showMore="ture"]').on('hover', function () {
-        console.log(this.getAttribute('name'))
+    $('div').on('click', 'button[showMore="true"]', function (e) {
+        e.preventDefault()
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+        let name = this.getAttribute('name'), pkid = this.getAttribute('pkid');
+        console.log(e.target, name, pkid)
+        // $('#scholarIfm').attr("src", "https://scholar.google.com/citations?view_op=search_authors&mauthors=" + name + "&hl=en&oi=ao")
+
+        // $('#scholarIfm').attr("src", "https://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=" + $('#customInput').val() + "&queryoption=TEACHER")
+
+        $('#scholarIfm').attr("src", "https://www.ratemyprofessors.com/ShowRatings.jsp?tid=" + pkid)
+
+        // $('#scholarIfm').attr("src", "https://search-production.ratemyprofessors.com/solr/rmp/select/?solrformat=true&rows=20&wt=json&json.wrf=noCB&callback=noCB&q=" + $('#customInput').val() + "&defType=edismax&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=total_number_of_ratings_i+desc&siteName=rmp&rows=20&start=0&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s&fq=")
+        $('#scholarIfm').toggle('fast');
     })
 }
 
